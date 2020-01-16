9caaf5f6b1574ed1567a28f175ee82ae98b2fb4e
EdoFede <18591616+EdoFede@users.noreply.github.com>
2020-01-16 10:40:28 +0100

    Automated test script

--- scripts/testOmada.sh
+++ scripts/testOmada.sh
@@ -23,7 +23,7 @@ docker create --name $CONTAINER --publish-all $IMAGE:$1
 logSubTitle "Starting test container"
 docker start $CONTAINER
 logSubTitle "Waiting for service to start"
-for i in {1..20}; do
+for i in {1..30}; do
 	echo -e ".\c"
 	sleep 1
 done
@@ -34,6 +34,8 @@ logSubTitle "Checking Omada startup"
 log=$(docker logs $CONTAINER 2>&1 |grep 'Omada Controller started')
 if [ "$log" != "Omada Controller started" ]; then
 	logError "Error: Omada controller is not starting"
+	logError "Logfile:"
+	logDetail "$(docker logs $CONTAINER 2>&1)"
 	logError "Aborting..."
 	cleanup
 	exit 1;
@@ -45,6 +47,8 @@ logSubTitle "Checking Omada web server"
 log=$(docker exec -ti $CONTAINER curl -v http://127.0.1.1:8088 |grep '302 Found')
 if [ "$log" != "< HTTP/1.1 302 Found" ]; then
 	logError "Error: web server is not responding properly"
+	logError "Logfile:"
+	logDetail "$(docker logs $CONTAINER 2>&1)"
 	logError "Aborting..."
 	cleanup
 	exit 1;
